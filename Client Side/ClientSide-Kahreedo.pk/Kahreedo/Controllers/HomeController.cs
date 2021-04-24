8d48f1d8daf315db0325a3a17e16bdc1fa058ac3
Murad1893 <muradmansoor189@gmail.com>
2021-04-24 16:26:34 +0500

    Fetched genMainSliders from Web API (first attempt)

--- Client Side/ClientSide-Kahreedo.pk/Kahreedo/Controllers/HomeController.cs
+++ Client Side/ClientSide-Kahreedo.pk/Kahreedo/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 ﻿using Khareedo.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,18 +16,25 @@ namespace Khareedo.Controllers
         KhareedoEntities db = new KhareedoEntities();
 
         // GET: Home
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            ViewBag.MenProduct = db.Products.Where(x => x.Category.Name.Equals("Men")).ToList();
-            ViewBag.WomenProduct = db.Products.Where(x => x.Category.Name.Equals("Women")).ToList();
-            ViewBag.SportsProduct = db.Products.Where(x => x.Category.Name.Equals("Sports")).ToList();
-            ViewBag.ElectronicsProduct = db.Products.Where(x => x.Category.Name.Equals("Phones")).ToList();
-            ViewBag.Slider = db.genMainSliders.ToList();
-            ViewBag.PromoRight = db.genPromoRights.ToList();
+            using (var client = new HttpClient())
+            {
+                ViewBag.MenProduct = db.Products.Where(x => x.Category.Name.Equals("Men")).ToList();
+                ViewBag.WomenProduct = db.Products.Where(x => x.Category.Name.Equals("Women")).ToList();
+                ViewBag.SportsProduct = db.Products.Where(x => x.Category.Name.Equals("Sports")).ToList();
+                ViewBag.ElectronicsProduct = db.Products.Where(x => x.Category.Name.Equals("Phones")).ToList();
 
-            this.GetDefaultData();
+                var response = await client.GetAsync("http://localhost:2509/api/genMainSliders");
+                string content = await response.Content.ReadAsStringAsync();
+                ViewBag.Slider = JsonConvert.DeserializeObject<IEnumerable<genMainSlider>>(content);
+                
+                ViewBag.PromoRight = db.genPromoRights.ToList();
 
-            return View();
+                this.GetDefaultData();
+                return View();
+            }
+
         }      
 
     }
